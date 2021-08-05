4820bf250b9e5aa4d82ce1e73b85e37a48b77898
yudong0114 <yukisaito114@gmail.com>
2021-08-06 01:47:13 +0900

    [add]css系のローダー追加

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,9 @@
 // hello.jsのインポート
 import { hello, goodnight} from "./hello";
+// index.cssのインポート
+import './index.css';
+// button.cssのインポート
+import './button.css';
 
 // helloボタン
 const hello_btn = document.getElementById('hello');
@@ -10,7 +14,7 @@ const goodnight_btn = document.getElementById('goodnight');
 hello_btn.addEventListener('click', () => {
   hello('山田 勝己');
 });
-// hellowボタンにリスナーの設定
+// helloボタンにリスナーの設定
 goodnight_btn.addEventListener('click', () => {
   goodnight('山田 勝己', 21);
 });
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // バンドルの構築を開始する場所
@@ -11,11 +12,11 @@ module.exports = {
   },
   // ファイル変更の検知の有効化
   watch: true,
-  // babelの使用(ES6 -> ES5)
   module: {
     rules: [
+      // babelの使用(ES6 -> ES5)
       {
-        test: /\.m?js$/,
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
@@ -23,11 +24,26 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      // cssの使用
+      {
+        test: /\.css/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { 
+            loader: 'css-loader', 
+            options: {
+              url: false,
+            }
+          },
+        ],
       }
     ]
   },
   plugins: [
     // HTML生成するプラグイン
-    new HtmlWebpackPlugin({template: './src/index.html'})
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    // CSSを別ファイルで出力するプラグイン
+    new MiniCssExtractPlugin({filename: 'index.css'})
   ]
 };
