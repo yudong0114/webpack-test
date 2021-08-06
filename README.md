0f857f0f976165e794b24e95e725819de71fe888
yudong0114 <yukisaito114@gmail.com>
2021-08-07 02:55:27 +0900

    [add]EJSの導入

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ $ docker exec -it コンテナID /bin/bash
 # 参考
 * Webpackの入門書  
   https://zenn.dev/yuri/books/4391b9280f823061932c
+* webpackでhtmlファイルも出力する
+  https://ema-hiro.hatenablog.com/entry/2017/10/12/015748
+* 【webpack 4】環境構築からJSとCSSを別出力まで（備忘録）  
+  https://www.expexp.jp/webpack/
+* webpackでEJSをコンパイルする方法  
+  https://haniwaman.com/webpack-ejs/
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,10 +4,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // バンドルの構築を開始する場所
-  entry: './src/',
+  entry: {
+    'index.js': './src/index.js',
+  },
   // バンドルしたファイルのエクスポート先
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   // ファイル変更の検知の有効化
@@ -37,13 +39,41 @@ module.exports = {
             }
           },
         ],
-      }
+      },
+      // ejsの使用
+      {
+				test: /\.ejs$/i,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: false
+						},
+					},
+					{
+						loader: 'ejs-plain-loader'
+					}
+				]
+			},
     ]
   },
   plugins: [
     // HTML生成するプラグイン
-    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({
+      // 書き出すHTML
+      filename: 'index.html',
+      // 指定するEJS
+			template : './src/index.ejs',
+      // minifyは行わない
+			minify: false,
+      // 読み込むファイル
+      chunks: [
+        'index.js',
+      ],
+    }),
     // CSSを別ファイルで出力するプラグイン
-    new MiniCssExtractPlugin({filename: 'index.css'})
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
   ]
 };
